0f1f341075377f44489fd3b489963fc02fe6900a
Bernd Beuster <bernd.beuster@gmail.com>
2016-05-26 15:38:10 +0200

    Added Python model.

---
+++ sim/python/cordic.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+# CORDIC in polar coordinates (rotating, vectoring)
+
+from math import sqrt, pi, cos, sin, atan, copysign
+
+def cordic (x0, y0, z0, vectoring = False, iterations = 10):
+    A = 1.0
+
+    # Input scaling and
+    # map argument -π...π to -π/2...π/2.
+    # 
+    # Replace <width> bit wide comparision operators
+    # with 2 bit comparision due to
+    # [0   ,  π/2) = 00...
+    # [π/2 ,  π  ) = 01...
+    # [0   , -π/2) = 11...
+    # [-π/2, -π  ) = 10...
+    if vectoring:
+        if x0 < 0 and y0 > 0:
+            x, y, z = y0, -x0, z0 + pi/2
+        elif x0 < 0 and y0 < 0:
+            x, y, z  = -y0, x0, z0 - pi/2
+        else:
+            x, y, z  = x0, y0, z0
+    else:
+        if z0 < -pi/2:
+            x, y, z  = y0, -x0, z0 + pi/2
+        elif z0 > pi/2:
+            x, y, z  = -y0, x0, z0 - pi/2
+        else:
+            x, y, z  = x0, y0, z0
+        
+    for i in xrange(iterations):
+        A = A * sqrt(1 + 2**(-2 * i))
+        pow2 = 2**(-i)
+
+        if vectoring:
+            d = -copysign(1, y)           
+        else:
+            d = copysign(1, z)
+          
+        x, y, z = x - y * d * pow2, y + x * d * pow2, z - d * atan(pow2)
+
+    return x, y, z, A
+
+
+#if __name__ == "__main__":
+
+bits = 10
+iterations = bits + 2
+N = bits
+
+
+# print format like ../behavioral/cos_sin/top.sv
+print "x, y, z"
+
+for i in xrange(2**N):
+    x0 = 2**(bits - 1) - 1
+    y0 = 0
+    z0 = ((i - 2**(N - 1)) * pi / 2**(N - 1))
+
+    x, y, z, A = cordic(x0, y0, z0, False, iterations)
+
+    #x, y = A * x0 * cos(z0), A * x0 * sin(z0)
+    x, y = x - A * x0 * cos(z0), y - A * x0 * sin(z0)
+
+    print "%5d, %5d, %5d" % (round(x), round(y), round(z))
