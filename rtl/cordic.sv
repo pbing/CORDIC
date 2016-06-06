00a37243cbc08c42ad9fafe561d067d55700c8b6
Bernd Beuster <bernd.beuster@gmail.com>
2016-06-06 20:32:22 +0200

    Added clock enable for FPGA clocking.

--- rtl/cordic.sv
+++ rtl/cordic.sv
@@ -12,6 +12,7 @@ module cordic
     guard_bits = iterations - 1)                  // equal to number of right shifts of x and y
    (input  wire                       reset,      // reset
     input  wire                       clk,        // clock
+    input  wire                       en,         // clock enable
     input  wire  signed [width - 1:0] x0, y0, z0, // inputs
     output logic signed [width    :0] x, y,       // outputs (scaled with K=1.6467...)
     output logic signed [width - 1:0] z);         // output (not scaled)
@@ -32,88 +33,89 @@ module cordic
             zr[i] <= '0;
          end
      else
-       for (int i = -1; i < iterations; ++i)
-         if (i == -1)
+       if (en)
+         for (int i = -1; i < iterations; ++i)
+           if (i == -1)
 
-	   /* Input scaling and
-	    * map argument -π...π to -π/2...π/2.
-            *
-            * Replace <width> bit wide comparision operators
-            * with 2 bit comparision due to
-            * [0   ,  π/2) = 00...
-            * [π/2 ,  π  ) = 01...
-            * [0   , -π/2) = 11...
-            * [-π/2, -π  ) = 10...
-	    */
-           if (vectoring == 1)
-             if (x0 < 0 && y0 >= 0) // z0 >= π/2
-               begin
-                  xr[0] <= y0          << guard_bits;
-                  yr[0] <= -x0         << guard_bits;
-                  zr[0] <= (z0 + pi_2) << guard_bits;
-               end
-             else if (x0 < 0 && y0 < 0) // z0 <= -π/2
-               begin
-                  xr[0] <= -y0         << guard_bits;
-                  yr[0] <= x0          << guard_bits;
-                  zr[0] <= (z0 - pi_2) << guard_bits;
-               end
+	     /* Input scaling and
+	      * map argument -π...π to -π/2...π/2.
+              *
+              * Replace <width> bit wide comparision operators
+              * with 2 bit comparision due to
+              * [0   ,  π/2) = 00...
+              * [π/2 ,  π  ) = 01...
+              * [0   , -π/2) = 11...
+              * [-π/2, -π  ) = 10...
+	      */
+             if (vectoring == 1)
+               if (x0 < 0 && y0 >= 0) // z0 >= π/2
+                 begin
+                    xr[0] <= y0          << guard_bits;
+                    yr[0] <= -x0         << guard_bits;
+                    zr[0] <= (z0 + pi_2) << guard_bits;
+                 end
+               else if (x0 < 0 && y0 < 0) // z0 <= -π/2
+                 begin
+                    xr[0] <= -y0         << guard_bits;
+                    yr[0] <= x0          << guard_bits;
+                    zr[0] <= (z0 - pi_2) << guard_bits;
+                 end
+               else
+                 begin
+                    xr[0] <= x0 << guard_bits;
+                    yr[0] <= y0 << guard_bits;
+                    zr[0] <= z0 << guard_bits;
+                 end
              else
-               begin
-                  xr[0] <= x0 << guard_bits;
-                  yr[0] <= y0 << guard_bits;
-                  zr[0] <= z0 << guard_bits;
-               end
+               if (z0[$left(z0)-:2] == 2'b10) // z0 <= -π/2
+                 begin
+                    xr[0] <= y0          << guard_bits;
+                    yr[0] <= -x0         << guard_bits;
+                    zr[0] <= (z0 + pi_2) << guard_bits;
+                 end
+               else if (z0[$left(z0)-:2] == 2'b01) // z0 >= π/2
+                 begin
+                    xr[0] <= -y0         << guard_bits;
+                    yr[0] <= x0          << guard_bits;
+                    zr[0] <= (z0 - pi_2) << guard_bits;
+                 end
+               else
+                 begin
+                    xr[0] <= x0 << guard_bits;
+                    yr[0] <= y0 << guard_bits;
+                    zr[0] <= z0 << guard_bits;
+                 end
            else
-             if (z0[$left(z0)-:2] == 2'b10) // z0 <= -π/2
-               begin
-                  xr[0] <= y0          << guard_bits;
-                  yr[0] <= -x0         << guard_bits;
-                  zr[0] <= (z0 + pi_2) << guard_bits;
-               end
-             else if (z0[$left(z0)-:2] == 2'b01) // z0 >= π/2
-               begin
-                  xr[0] <= -y0         << guard_bits;
-                  yr[0] <= x0          << guard_bits;
-                  zr[0] <= (z0 - pi_2) << guard_bits;
-               end
-             else
-               begin
-                  xr[0] <= x0 << guard_bits;
-                  yr[0] <= y0 << guard_bits;
-                  zr[0] <= z0 << guard_bits;
-               end
-         else
-           if (vectoring == 1)
+             if (vectoring == 1)
 
-             /* Vectoring Mode */
-             if (yr[i] >= 0)
-               begin
-                  xr[i + 1] <= xr[i] + (yr[i] >>> i);
-                  yr[i + 1] <= yr[i] - (xr[i] >>> i);
-                  zr[i + 1] <= zr[i] + atan_z[i];
-               end
+               /* Vectoring Mode */
+               if (yr[i] >= 0)
+                 begin
+                    xr[i + 1] <= xr[i] + (yr[i] >>> i);
+                    yr[i + 1] <= yr[i] - (xr[i] >>> i);
+                    zr[i + 1] <= zr[i] + atan_z[i];
+                 end
+               else
+                 begin
+                    xr[i + 1] <= xr[i] - (yr[i] >>> i);
+                    yr[i + 1] <= yr[i] + (xr[i] >>> i);
+                    zr[i + 1] <= zr[i] - atan_z[i];
+                 end
              else
-               begin
-                  xr[i + 1] <= xr[i] - (yr[i] >>> i);
-                  yr[i + 1] <= yr[i] + (xr[i] >>> i);
-                  zr[i + 1] <= zr[i] - atan_z[i];
-               end
-           else
 
-             /* Rotating Mode */
-             if (zr[i] < 0)
-               begin
-                  xr[i + 1] <= xr[i] + (yr[i] >>> i);
-                  yr[i + 1] <= yr[i] - (xr[i] >>> i);
-                  zr[i + 1] <= zr[i] + atan_z[i];
-               end
-             else
-               begin
-                  xr[i + 1] <= xr[i] - (yr[i] >>> i);
-                  yr[i + 1] <= yr[i] + (xr[i] >>> i);
-                  zr[i + 1] <= zr[i] - atan_z[i];
-               end
+               /* Rotating Mode */
+               if (zr[i] < 0)
+                 begin
+                    xr[i + 1] <= xr[i] + (yr[i] >>> i);
+                    yr[i + 1] <= yr[i] - (xr[i] >>> i);
+                    zr[i + 1] <= zr[i] + atan_z[i];
+                 end
+               else
+                 begin
+                    xr[i + 1] <= xr[i] - (yr[i] >>> i);
+                    yr[i + 1] <= yr[i] + (xr[i] >>> i);
+                    zr[i + 1] <= zr[i] - atan_z[i];
+                 end
 
    /* output register */
    always_ff @(posedge clk or posedge reset)
@@ -124,11 +126,12 @@ module cordic
           z <= '0;
        end
      else
-       begin
-          x <= round_result(xr[iterations]);
-          y <= round_result(yr[iterations]);
-          z <= round_result(zr[iterations]);
-       end
+       if (en)
+         begin
+            x <= round_result(xr[iterations]);
+            y <= round_result(yr[iterations]);
+            z <= round_result(zr[iterations]);
+         end
 
    function signed [width:0] round_result(input signed [width + guard_bits:0] x);
       return (x + 2**(guard_bits - 1)) >>> guard_bits;
